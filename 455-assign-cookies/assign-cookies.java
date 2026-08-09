ba6cc2d0f4e31a971312a18df66eb3192925f9d0
himanshu-treasures-09 <himanshum.9335@gmail.com>
2026-08-09 09:40:02 +0530

    Time: 15 ms (59.87%) | Memory: 51.5 MB (56.36%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int left = 0;
+        int right = 0;
+        int count = 0;
+        while(left<g.length && right<s.length){
+            if(g[left]<=s[right]){
+                count++;
+                left++;
+                right++;
+            }else{
+                right++;
+            }
+        }
+        return count;
+    }
+}
